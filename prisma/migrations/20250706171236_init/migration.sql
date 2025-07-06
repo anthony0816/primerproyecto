3fa69f6597ab91f182781a58fcc3eecf874c38ad
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-07-06 14:12:45 -0400

    haciendo los modelos, en proceso todavia de hacer los modelos

---
+++ prisma/migrations/20250706171236_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Perro" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "PerroAmarillo" (
+    "identificador" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "almoada" BOOLEAN NOT NULL
+);
